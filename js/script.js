92907b92022b64a52276ea3dcbe61971b15ee054
Muhammad Shafly Putra Darmawan <pojokbelajarkuid@gmail.com>
2022-02-20 11:40:21 +0800

    First Commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Init AOS
+AOS.init();
+
+window.onscroll = function () {
+    const navbar = document.getElementsByClassName('navbar')[0];
+    // if document scrolled, add 'bg-dark' & 'shadow-sm' classes to navbar
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+        navbar.classList.add('bg-dark', 'shadow-sm');
+    } else {
+        navbar.classList.remove('bg-dark', 'shadow-sm');
+    }
+
+
+}
